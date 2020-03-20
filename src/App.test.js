fed83ee4fd03d42b589d7c1f9cb8d46d16c21d91
Ben <bvanhou@gmail.com>
2020-03-20 10:49:54 -0400

    wip

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { mount } from 'enzyme';
-import App from './App';
+import Home from './views/Home';
 
 describe('App', () => {
 
   it('renders generate text button', () => {
-    const { getByText } = render(<App />);
+    const { getByText } = render(<Home />);
     const buttonElement = getByText(/Generate Text/i);
     expect(buttonElement).toBeInTheDocument();
     
   });
 
   it('Button Click Generates new Text', () => {
-    const wrapper = mount(<App />);
+    const wrapper = mount(<Home />);
     const _prev = wrapper.find('p').text();
     wrapper.find("button").simulate("click");
     expect(wrapper.find('p').text()).not.toEqual(_prev);
